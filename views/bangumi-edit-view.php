0e71b85466a86c58b519c5cd192649fbcc208f9a
SamLangTen <samlangten@outlook.com>
2020-08-19 22:07:58 +0800

    add delete confirm page

--- views/bangumi-edit-view.php
+++ views/bangumi-edit-view.php
@@ -5,9 +5,38 @@
         return;
     }
     if ($_GET['action']=='delete') {
-        bangumi::delete_bangumi_from_id($_GET['bangumi_id']);
-        redirect_to_admin_url("admin.php?page=sinon_bangumi_list", null);
+        $bangumi = bangumi::get_bangumi_by_id($_GET['bangumi_id']);
+        render_delete_box($bangumi);
     } else {
         $bangumi = bangumi::get_bangumi_by_id($_GET['bangumi_id']);
         include_once(ROOT_PATH."/views/bangumi-edit-component.php");
+    }
+
+    if ($_POST['action']=='apply_delete') {
+        $ids = $_POST['bangumi_id'];
+        if ($ids != null) {
+            foreach ($ids as $id) {
+                bangumi::delete_bangumi_from_id($id);
+            }
+        }
+        redirect_to_admin_url("admin.php?page=sinon_bangumi_list", null);
+    }
+
+    function render_delete_box($bangumi)
+    {
+        ?>
+        <form action="" method="POST">
+            <div class="wrap">
+            <h1><?php _e("Delete bangumi", "sinon-bangumi-list") ?></h1>
+            <p><?php _e("You are going to delete this bangumi", "sinon-bangumi-list") ?></p>	
+            <ul>
+                <li><input type="hidden" name="bangumi_id[]" value="<?php echo($bangumi['id']); ?>">ID <?php echo($bangumi['id']); ?>：<?php echo($bangumi['name_cn']); ?></li>
+            </ul>
+                <input type="hidden" name="action" value="apply_delete">
+                    <p class="submit">
+                        <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e("Delete", "sinon-bangumi-list") ?>"/>
+                    </p>
+            </div>
+        </form>
+        <?php
     }
