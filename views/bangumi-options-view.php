59f1358ad29ed3ed03a5201768f87b4734dafb66
SamLangTen <samlangten@outlook.com>
2020-08-27 16:42:20 +0800

    add page header to options page

--- views/bangumi-options-view.php
+++ views/bangumi-options-view.php
@@ -22,46 +22,49 @@
     }
     
 ?>
-<form action="" method="POST">
-    <table class="form-table">
-        <input name="action" value="do_edit" type="hidden"/>
-        <input name="bangumi_id" value="<?php echo($bangumi['id']); ?>" type="hidden"/>
-        <tbody>
-            <!--Display Mode-->
-            <tr>
-                <th scope="row"><label for="display_mode"><?php _e("Image URL", "sinon-bangumi-list"); ?></label></th>
-                <td>
-                    <fieldset>
-                        <label>
-                            <input type="radio" name="display_mode" value="comment" <?php echo($display_mode=="comment"?"checked='checked'":""); ?> />
-                            <span><?php _e("Comment Mode", "sinon-bangumi-list"); ?></span>
-                        </label>
-                        <br/>
-                        <label>
-                            <input type="radio" name="display_mode" value="list"<?php echo($display_mode=="list"?"checked='checked'":""); ?>/>
-                            <span><?php _e("List Mode", "sinon-bangumi-list"); ?></span>
-                        </label>
-                    </fieldset>
-                </td>
-            </tr>    
-            <!--Sort Mode-->
-            <tr>
-                <th scope="row"><label for="sort_mode"><?php _e("Sort Mode", "sinon-bangumi-list"); ?></label></th>
-                <td>
-                    <fieldset>
-                        <label>
-                            <input type="radio" name="sort_mode" value="update_time" <?php echo($sort_mode=="update_time"?"checked='checked'":""); ?> />
-                            <span><?php _e("Sort by Update Time", "sinon-bangumi-list"); ?></span>
-                        </label>
-                        <br/>
-                        <label>
-                            <input type="radio" name="sort_mode" value="id"<?php echo($sort_mode=="id"?"checked='checked'":""); ?>/>
-                            <span><?php _e("Sort by Id", "sinon-bangumi-list"); ?></span>
-                        </label>
-                    </fieldset>
-                </td>
-            </tr>                                                                     
-        </tbody>
-    </table>
-    <?php submit_button(); ?>
-</form>
+<div class="wrap">
+    <h1><?php _e("Sinon Bangumi List Options","sinon-bangumi-list"); ?></h1>
+    <form action="" method="POST">
+        <table class="form-table">
+            <input name="action" value="do_edit" type="hidden"/>
+            <input name="bangumi_id" value="<?php echo($bangumi['id']); ?>" type="hidden"/>
+            <tbody>
+                <!--Display Mode-->
+                <tr>
+                    <th scope="row"><label for="display_mode"><?php _e("Image URL", "sinon-bangumi-list"); ?></label></th>
+                    <td>
+                        <fieldset>
+                            <label>
+                                <input type="radio" name="display_mode" value="comment" <?php echo($display_mode=="comment"?"checked='checked'":""); ?> />
+                                <span><?php _e("Comment Mode", "sinon-bangumi-list"); ?></span>
+                            </label>
+                            <br/>
+                            <label>
+                                <input type="radio" name="display_mode" value="list"<?php echo($display_mode=="list"?"checked='checked'":""); ?>/>
+                                <span><?php _e("List Mode", "sinon-bangumi-list"); ?></span>
+                            </label>
+                        </fieldset>
+                    </td>
+                </tr>    
+                <!--Sort Mode-->
+                <tr>
+                    <th scope="row"><label for="sort_mode"><?php _e("Sort Mode", "sinon-bangumi-list"); ?></label></th>
+                    <td>
+                        <fieldset>
+                            <label>
+                                <input type="radio" name="sort_mode" value="update_time" <?php echo($sort_mode=="update_time"?"checked='checked'":""); ?> />
+                                <span><?php _e("Sort by Update Time", "sinon-bangumi-list"); ?></span>
+                            </label>
+                            <br/>
+                            <label>
+                                <input type="radio" name="sort_mode" value="id"<?php echo($sort_mode=="id"?"checked='checked'":""); ?>/>
+                                <span><?php _e("Sort by Id", "sinon-bangumi-list"); ?></span>
+                            </label>
+                        </fieldset>
+                    </td>
+                </tr>                                                                     
+            </tbody>
+        </table>
+        <?php submit_button(); ?>
+    </form>
+</div>
